964de4ea621a0b31bbb4778bc92aaf2ce2088c7d
LarryLin35 <good.cool112233@gmail.com>
2023-07-31 02:12:10 +0100

    Add MyCamera and other

---
+++ MyCamera/camera_test_01.py
@@ -0,0 +1,16 @@
+import cv2
+
+if __name__ == "__main__":
+    cap = cv2.VideoCapture('nvarguscamerasrc ! video/x-raw(memory:NVMM), width=3280, height=2464, format=(string)NV12, framerate=(fraction)20/1 ! nvvidconv flip-method=0 ! video/x-raw, width=(int)1280, height=(int)720, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink')
+    
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        cv2.imshow('Live Video', frame) 
+        if cv2.waitKey(1) == ord('q'): 
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
